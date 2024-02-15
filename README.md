05edafedcdec31cf215015fa3d8ad0716ef70051
Rickyxrc <rickyxrc@outlook.com>
2024-02-15 11:05:48 +0800

    feat(server.py): add command feature

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 用法的话还没准备写文档，看看源代码吧，也不长。
 
+
+## 安装
+
+首先您需要正确地在 OBS 中安装 Echo-live。
+
+然后定位到 `config.js` 的 websocket 这部分，将 `websocket_enable` 改为 `true`。
+
+下方的 `websocket_url` 中请填写服务端的 ip 地址与端口号。（如果您在一台电脑上同时运行 OBS 和本程序则 ip 可以写 `127.0.0.1`）
+
+```js
+        // 启用 WebSocket
+        // * 如果没人要求您这么做，请不要动它。
+        // * 广播模式下启用 WebSocket 可连接至服务器以从第三方软件获取消息。
+        // * 可从服务器接收的消息和广播消息一致，发送的消息须使用类似于 JSON.stringify 的方法序列化。
+        // * 详见：https://sheep-realms.github.io/Echo-Live-Doc/dev/broadcast/
+        websocket_enable: true,
+        // WebSocket 连接地址
+        // websocket_url: 'ws://192.168.1.12:3000', // iPad
+        websocket_url: 'ws://127.0.0.1:3000',
+```
+
+如果您在两台设备上分别运行 OBS 和本程序，请务必将 `server.py` 的 `host = "127.0.0.1"` 修改为 `host = "0.0.0.0"`（监听局域网）。
+
+## 配置
+
+还没写。
+
--- server.py
+++ server.py
@@ -14,12 +14,14 @@ def parse_message(msg: str) -> str:
     """
     解析文字输入，传出为 echo 程序可以理解的文本
     """
+    # TODO: 1. 拆分模块
+    # TODO: 2. 制定一套文本输入规范
     # 目前先返回单句话，这个逻辑待定
     return json.dumps(
         {
             "action": "message_data",
             "data": {
-                "username": "还在开发急什么",
+                "username": username,
                 "messages": [
                     {"message": msg},
                 ],
@@ -28,7 +30,12 @@ def parse_message(msg: str) -> str:
     )
 
 
+# TODO: 用配置文件来改这些，别修改源代码
 COMMAND_PREFIX = "/"
+# pylint: disable=invalid-name
+username = "？？？"
+HOST = "127.0.0.1"
+PORT = 3000
 
 console = Console()
 
@@ -65,6 +72,7 @@ async def get_message(websocket, cid):
             # await websocket.send(json.dumps({
             # "action": "echo_next",
             # }))
+
         elif data["action"] == "close":
             response += "发出下线请求"
         elif data["action"] == "page_hidden":
@@ -76,7 +84,7 @@ async def get_message(websocket, cid):
         console.log(response)
 
 
-async def listen_queue(websocket, _cid):
+async def listen_queue(websocket, cid):
     """
     监听事件队列，如果有啥事情就执行
     """
@@ -86,12 +94,12 @@ async def listen_queue(websocket, _cid):
         if proceed < len(events):
             for event in events[proceed:]:
                 # print(event)
-                console.log(f"客户端{client_id}: 执行 {event}")
+                console.log(f"客户端{cid}: 执行 {event}")
                 if event["action"] == "message_data":
-                    console.log(f"客户端{client_id}: 发送文字信息")
+                    console.log(f"客户端{cid}: 发送文字信息")
                     await websocket.send(event["data"])
                 else:
-                    console.log(f"[red]客户端{client_id}: 上面那个事件的实现程序还没写呢！执行不了！[/red]")
+                    console.log(f"[red]客户端{cid}: 上面那个事件的实现程序还没写呢！执行不了！[/red]")
             proceed = len(events)
 
 
@@ -100,36 +108,58 @@ async def echo(websocket, _path):
     websocket 连接上就执行这个
     """
     # pylint: disable=global-statement
-    global client_id
-    client_id += 1
-    cid = client_id
-    console.log(f"客户端{client_id}: 调用了一个echo函数")
-    await asyncio.create_task(listen_queue(websocket, cid))
-    await asyncio.create_task(get_message(websocket, cid))
+    try:
+        global client_id
+        client_id += 1
+        cid = client_id
+        console.log(f"客户端{client_id}: 已建立连接")
+        listen = asyncio.create_task(listen_queue(websocket, cid))
+        get = asyncio.create_task(get_message(websocket, cid))
+        await listen
+        await get
+    except websockets.exceptions.ConnectionClosedOK:
+        console.log(f"客户端{client_id}: 连接已断开")
 
 
 async def run_input():
     """
     获取用户输入，执行命令
     """
+    # pylint: disable=global-statement
+    global username
     session = PromptSession()
     while True:
         with patch_stdout(raw=True):
             input_data = await session.prompt_async("请输入命令: ")
-        if input_data[0] != COMMAND_PREFIX:
+        # TODO: 这个太简陋了，考虑用 argparse 之类的库在这里解析命令
+        if input_data == "":
+            console.log("[red]打个字再回车啊宝！[/red]")
+        elif input_data[0] != COMMAND_PREFIX:
             console.log(f"发送文字消息: {input_data}")
             events.append({"action": "message_data", "data": parse_message(input_data)})
         else:
             console.log(f"执行命令：{input_data}")
-            console.log("[red]这个功能还没做好，不急！！！！[/red]")
+            commands = input_data.split(" ")
+            if commands[0][1:] in ["rename", "ren"]:
+                if len(commands) == 2:
+                    console.log(f"[green]已经将显示名称更改为 {commands[1]}[/green]")
+                    username = commands[1]
+                else:
+                    console.log("[red]命令接受一个参数，不多不少。[/red]")
+            elif commands[0][1:] in ["quit", "q"]:
+                console.log("拜拜~")
+                # TODO: 消掉退出时的错误提示
+                raise SystemExit(3)  # 就是用来退出的别见怪
+            else:
+                console.log(
+                    f"[red]用 COMMAND_PREFIX（当前为'{COMMAND_PREFIX}'）开头的消息"
+                    "将会被作为命令解析，这个功能还没做好，不急！！！！[/red]"
+                )
 
 
-host = "127.0.0.1"
-port = 3000
+asyncio.get_event_loop().run_until_complete(websockets.serve(echo, HOST, PORT))
 
-asyncio.get_event_loop().run_until_complete(websockets.serve(echo, host, port))
-
-console.log(f"[green]已经在 {host}:{port} 监听 websocket 请求，等待 echo 客户端接入...[/green]")
+console.log(f"[green]已经在 {HOST}:{PORT} 监听 websocket 请求，等待 echo 客户端接入...[/green]")
 console.log("[blue]tips: 如果没有看到成功的连接请求，可以尝试刷新一下客户端[/blue]")
 
 asyncio.get_event_loop().create_task(run_input())
